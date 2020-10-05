df7c2afe78e132d9b2c7b86474e3b738aa8d21a6
Changgyun Ma <caggun1210@gmail.com>
2020-10-06 02:49:23 +0900

    Update 4871. 4일차 그래프 경로.py
    deleted all prints cuz NOW I know how to use debugging!

--- SWExpert/Intermadiate/4871. 4일차 그래프 경로.py
+++ SWExpert/Intermadiate/4871. 4일차 그래프 경로.py
@@ -75,48 +75,26 @@ E개의 줄 이후에는 경로의 존재를 확인할 출발 노드 S와 도착
 '''
 
 def DSF(v):
-    print("visited in DSF Before: ", visited)
-    print("v in DSF : ", v)
     visited[v] = False
-    print("visited in DSF After: ", visited)
-    print("visited[v] in DSF : ", visited[v])
-    print("graph[v] in DSF : ", graph[v])
     for i in graph[v]:
-        print("i in DSF in for loop : ", i)
-        print("visited[i] : ", visited[i])
         if visited[i]:
-            print('recurse DSF')
-            print()
             DSF(i)
 
 T = int(input())
 for t in range(T):
     V, E = map(int, input().split())
-    print("V : {0}, E : {1}".format(V, E))
     stack= []
     graph = [[] for _ in range(V+1)]
     visited = [True for _ in range(V+1)]
     for i in range(E):
         a, b = map(int, input().split())
-        print("a : {0}, b : {1}".format(a, b))
         graph[a].append(b) # 단방향 그래프이므로 하나만 추가한다.
-        print("graph : ", graph)
-        print("graph[a] : ", graph[a])
-    print("\n")
     start, end = map(int, input().split())
-    print("Start : {0}, End : {1}".format(start, end))
-    print("\n")
     DSF(start)
     result = 1
 
-    print('\n')
-    #끝나는 노드를 못갔다면 0으로 결과를 바꾼다.
-    print("end : ", end)
-    print('visited : ', visited)
-    print('visited[end] : ', visited[end])
     if visited[end]:
         result = 0
-    print("result : ", result)
     print("#{} {}".format(t+1, result))
 
 
@@ -148,4 +126,39 @@ for t in range(T):
         marcov_2 = np.dot(marcov_1, marcov_2)
 
     print("#{0} {1}".format(t+1, result))
+    '''
+
+'''
+선용 알고리즘
+'''
+'''
+def DFS(START):
+    visited[START] = True#방문리스트에 시작노드를 넣어준다.
+    result = 0
+    if START in my_map: # 시작하는 노드가 있다면. 즉 없으면 result는 0으로 반환한다
+        for NEXT in my_map[START]:#S에서 연결되는 노드들을 하나씩 NEXT에 넣어서 G와 맞는지 비교한다.
+            if not visited[NEXT]:#방문리스트에 NEXT에 해당하는 값이 false라면
+                if NEXT == G:
+                    return 1 # S,G가 연결이 가능하기 때문에 1을 반환시켜준다.
+                elif not result: # 결과가 안났다면 계속 탐색
+                    result += DFS(NEXT)#S의 노드에서 이어지는 노드에서 도착노드(G)까지 이어질 수 있는 확인
+    return result
+
+T = int(input())
+for test_case in range(1, T + 1):
+    V, E = map(int, input().split())
+    
+    my_map = {} # 딕셔너리
+    visited = [False for _ in range(V + 1)]#노드애서 연결된 도착 노드의 리스트
+    result = 0
+    
+    for _ in range(E):
+        Start, End = map(int, input().split())
+        if Start not in my_map:#시작노드에서 도착하는 노드의 리스트를 value값으로 넣는다
+            my_map[Start] = [End]
+        else:
+            my_map[Start].append(End)
+        
+    S, G = map(int, input().split())
+    print('#{} {}'.format(test_case, DFS(S)))
     '''
